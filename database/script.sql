e70c7400fb293ae4aed78d1f65abc56c50ab7114
Nikita Sakharin <17588081+nikita-sakharin@users.noreply.github.com>
2023-10-06 22:25:21 +0300

    Update script.sql

--- database/script.sql
+++ database/script.sql
@@ -1,16 +1,18 @@
 -- sudo --user=postgres psql --file=script.sql
 DROP DATABASE IF EXISTS employees;
-DROP USER IF EXISTS employees_user;
+DROP USER IF EXISTS employees;
 
 -- Just for example. You shouldn't save password to git.
-CREATE USER employees_user WITH
-    UNENCRYPTED PASSWORD 'a2{O)9U%V+Lt~dnMykSPG1?sF8Wf:p0H';
+CREATE USER employees WITH
+    NOSUPERUSER
+    NOCREATEDB
+    NOCREATEROLE
+    ENCRYPTED PASSWORD 'a2{O)9U%V+Lt~dnMykSPG1?sF8Wf:p0H';
 CREATE DATABASE employees WITH
-    OWNER employees_user
-    ENCODING 'UTF8';
+    OWNER = employees_user
+    ENCODING = 'UTF8';
 
--- psql --dbname=employees --username=employees_user --password
-
+-- psql --dbname=employees --username=employees --password
 COPY employee(id, supervisor, full_name)
     FROM 'employee.csv'
         WITH DELIMITER AS ',' NULL AS  '0' CSV HEADER ENCODING 'UTF8';
